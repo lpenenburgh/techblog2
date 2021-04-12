f9e802473157411fffd0ab2f010c031c204dab32
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-04-11 20:34:06 -0600

    redo

---
+++ controllers/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+//dashboard route?
+
+
+
+
+module.exports = router;
